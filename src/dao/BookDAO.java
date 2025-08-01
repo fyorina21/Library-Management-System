0d36b7e867902426e257aea7c9adb5e1b149205a
Rahel Agonafer <rahelagonafer@gmail.com>
2025-08-01 03:12:10 -0700

    Update BookDAO implementation

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -1,36 +1,6 @@
 package dao;
 
 import java.io.BufferedInputStream;
-
-import model.Book;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-public class BookDAO {
-    private static final String FILE_PATH = "book.dat";
-
-    public void saveBook (Book book) {
-        List<Book> books = loadAllBooks();
-        books.add(book);
-        try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream (new FileOutputStream(FILE_PATH)))){
-            out.writeObject(books);
-        }catch (IOException e) {
-            System.out.println ("Error: " + e.getMessage());
-        }
-
-        public List<Book> loadAllBooks() {
-        File file = new File(FILE_PATH);
-    public List<Book> loadAllBooks() {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) return new ArrayList<>();
-        try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(FILE_PATH)))) {
-            return (List<Book>) in.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error : " + e.getMessage());
-            return new ArrayList<>();
-package dao;
-
-import java.io.BufferedInputStream;
 import model.Book;
 import java.io.*;
 import java.util.ArrayList;
@@ -49,11 +19,18 @@ public class BookDAO {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public List<Book> loadAllBooks() {
         File file = new File(FILE_PATH);
         if (!file.exists()) return new ArrayList<>();
         try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(FILE_PATH)))) {
-            return (List<Book>) in.readObject();
+            Object obj = in.readObject();
+            if (obj instanceof List<?>) {
+                return (List<Book>) obj;
+            } else {
+                System.out.println("Error : Data in file is not a List");
+                return new ArrayList<>();
+            }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("Error : " + e.getMessage());
             return new ArrayList<>();
